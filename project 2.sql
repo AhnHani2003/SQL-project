1cb67559cbfaff2da4877e9e342843246916cf51
Trần Thông <145345242+AhnHani2003@users.noreply.github.com>
2024-04-12 19:42:58 +0700

    Update project 2.sql

--- project 2.sql
+++ project 2.sql
@@ -73,7 +73,8 @@ ORDER BY dates, product_categories;
 
 
 
------
+
+--Tạo metric trước khi dựng dashboard
 --TPV
 WITH TPV AS(
   SELECT 
@@ -149,12 +150,12 @@ SELECT
     MR.tpv AS TPV,
     MO.tpo AS TPO,
     CASE 
-        WHEN LAG(MR.tpv) OVER (ORDER BY MR.Year, MR.Month) IS NULL THEN 0
-        ELSE ROUND((MR.tpv - LAG(MR.tpv) OVER (ORDER BY MR.Year, MR.Month)) / LAG(MR.tpv) OVER (ORDER BY MR.Year, MR.Month),2)
+        WHEN LAG(MR.tpv) OVER (ORDER BY MR.Year, MR.Month) IS NULL THEN 0 || '%'
+        ELSE ROUND((MR.tpv - LAG(MR.tpv) OVER (ORDER BY MR.Year, MR.Month)) / LAG(MR.tpv) OVER (ORDER BY MR.Year, MR.Month),2) || '%'
     END AS Revenue_growth,
     CASE 
-        WHEN LAG(MO.tpo) OVER (ORDER BY MO.Year, MO.Month) IS NULL THEN 0
-        ELSE ROUND((MO.tpo - LAG(MO.tpo) OVER (ORDER BY MO.Year, MO.Month)) / LAG(MO.tpo) OVER (ORDER BY MO.Year, MO.Month),2)
+        WHEN LAG(MO.tpo) OVER (ORDER BY MO.Year, MO.Month) IS NULL THEN 0 || '%'
+        ELSE ROUND((MO.tpo - LAG(MO.tpo) OVER (ORDER BY MO.Year, MO.Month)) / LAG(MO.tpo) OVER (ORDER BY MO.Year, MO.Month),2) || '%'
     END AS Order_growth,
     TC.Total_cost AS Total_cost,
     TP.Total_profit AS Total_profit,
@@ -175,3 +176,87 @@ ORDER BY
 
 
 
+--Cohort
+WITH retail AS(
+    SELECT a.*, b.*, c.*, d.*
+    FROM bigquery-public-data.thelook_ecommerce.orders a
+    JOIN bigquery-public-data.thelook_ecommerce.order_items b ON a.order_id = b.order_id AND a.user_id = b.user_id
+    JOIN bigquery-public-data.thelook_ecommerce.users c ON  a.user_id = c.id
+    JOIN bigquery-public-data.thelook_ecommerce.products d ON b.product_id = d.id
+    WHERE c.id <> ''
+    AND a.num_of_item > 0
+),
+retail_main AS (
+    SELECT * FROM (
+        SELECT *, ROW_NUMBER() OVER(PARTITION BY order_id, product_id, num_of_item ORDER BY created_at) as STT
+        FROM retail
+    ) AS t
+    WHERE STT =1
+), 
+retail_index AS (
+    SELECT user_id, sale_price, created_at
+    FORMAT_DATE('%Y-%m', created_at) AS corhort_date
+    ((EXTRACT('year' FROM created_at) - EXTRACT('year' FROM first_purchase_date)) * 12) + ((EXTRACT('month' FROM created_at)) - EXTRACT('month' FROM first_purchase_date) +1) AS index
+    FROM(SELECT user_id, sale_price, 
+        MIN(created_at) OVER(PARTITION BY user_id) AS first_purchase_date,
+        created_at
+        FROM retail_main) a
+),
+XXX AS (
+    SELECT cohort_date, index,
+    COUNT(DISTINCT user_id) as cnt,
+    SUM(sale_price) as revenue
+    FROM retail_index
+    GROUP BY cohort_date, index
+),
+--customer_cohort
+customer_cohort AS (SELECT 
+cohort_date,
+SUM(CASE WHEN index = 1 THEN cnt ELSE 0 END) as m1,
+SUM(CASE WHEN index = 2 THEN cnt ELSE 0 END) as m2,
+SUM(CASE WHEN index = 3 THEN cnt ELSE 0 END) as m3,
+SUM(CASE WHEN index = 4 THEN cnt ELSE 0 END) as m4,
+SUM(CASE WHEN index = 5 THEN cnt ELSE 0 END) as m5,
+SUM(CASE WHEN index = 6 THEN cnt ELSE 0 END) as m6,
+SUM(CASE WHEN index = 7 THEN cnt ELSE 0 END) as m7,
+SUM(CASE WHEN index = 8 THEN cnt ELSE 0 END) as m8,
+SUM(CASE WHEN index = 9 THEN cnt ELSE 0 END) as m9,
+SUM(CASE WHEN index = 10 THEN cnt ELSE 0 END) as m10,
+SUM(CASE WHEN index = 11 THEN cnt ELSE 0 END) as m11,
+SUM(CASE WHEN index = 12 THEN cnt ELSE 0 END) as m12,
+SUM(CASE WHEN index = 13 THEN cnt ELSE 0 END) as m13
+FROM xxx
+GROUP BY cohort_date
+ORDER BY cohort_date)
+--retention_cohort
+SELECT cohort_date,
+ROUND(100.00*m1/m1,2) || "%" m1,
+ROUND(100.00*m2/m1,2) || "%" m2,
+ROUND(100.00*m3/m1,2) || "%" m3,
+ROUND(100.00*m4/m1,2) || "%" m4,
+ROUND(100.00*m5/m1,2) || "%" m5,
+ROUND(100.00*m6/m1,2) || "%" m6,
+ROUND(100.00*m7/m1,2) || "%" m7,
+ROUND(100.00*m8/m1,2) || "%" m8,
+ROUND(100.00*m9/m1,2) || "%" m9,
+ROUND(100.00*m10/m1,2) || "%" m10,
+ROUND(100.00*m11/m1,2) || "%" m11,
+ROUND(100.00*m12/m1,2) || "%" m12,
+ROUND(100.00*m13/m1,2) || "%" m13
+FROM customer_cohort
+--churn_cohort
+SELECT cohort_date,
+ROUND(100.00-(100.00*m1/m1),2) || "%" m1,
+ROUND(100.00-(100.00*m2/m1),2) || "%" m2,
+ROUND(100.00-(100.00*m3/m1),2) || "%" m3,
+ROUND(100.00-(100.00*m4/m1),2) || "%" m4,
+ROUND(100.00-(100.00*m5/m1),2) || "%" m5,
+ROUND(100.00-(100.00*m6/m1),2) || "%" m6,
+ROUND(100.00-(100.00*m7/m1),2) || "%" m7,
+ROUND(100.00-(100.00*m8/m1),2) || "%" m8,
+ROUND(100.00-(100.00*m9/m1),2) || "%" m9,
+ROUND(100.00-(100.00*m10/m1),2) || "%" m10,
+ROUND(100.00-(100.00*m11/m1),2) || "%" m11,
+ROUND(100.00-(100.00*m12/m1),2) || "%" m12,
+ROUND(100.00-(100.00*m13/m1),2) || "%" m13
+FROM customer_cohort
